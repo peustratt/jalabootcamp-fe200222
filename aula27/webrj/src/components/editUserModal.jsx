eaf54966a2714b2fb2cfebca58c2bdcbee99e7b0
pedro-ivo <peustratt@gmail.com>
2023-01-17 19:25:19 -0300

    styled edit user Modal

--- aula27/webrj/src/components/editUserModal.jsx
+++ aula27/webrj/src/components/editUserModal.jsx
@@ -44,27 +44,27 @@ const EditUserModal = ({ id, setEditModal }) => {
         onClick={() => setEditModal({ isOpen: false, id: null })}
       ></div>
       <form
-        className="flex flex-col w-fit bg-[rgba(150,127,0,0.685)] z-10 p-4 rounded-md"
+        className="flex flex-col justify-center items-center gap-4 w-1/2 max-w-[400px] min-w-[200px] bg-[rgba(150,127,0,0.685)] z-10 p-4 rounded-md"
         onSubmit={submit}
       >
         <input
           value={user.name}
           onChange={(e) => setUser({ ...user, name: e.target.value })}
-          className="text-field"
+          className="rounded-[4px] w-full indent-1 h-[1.7rem]"
           type={"text"}
           placeholder="name"
         />
         <input
           value={user.email}
           onChange={(e) => setUser({ ...user, email: e.target.value })}
-          className="text-field"
+          className="rounded-[4px] w-full indent-1 h-[1.7rem]"
           type={"email"}
           placeholder="email"
         />
         <select
           value={user.type}
           onChange={(e) => setUser({ ...user, type: e.target.value })}
-          className="text-field"
+          className="rounded-[4px] w-full indent-1 h-[1.7rem]"
           type={"select"}
           placeholder="user type"
         >
@@ -73,7 +73,14 @@ const EditUserModal = ({ id, setEditModal }) => {
           <option value="seller">Seller</option>
           <option value="buyer">Buyer</option>
         </select>
-        <button className="btn mt-5">Edit</button>
+        <div className="flex p-4 box-border w-full gap-2">
+          <button className="btn mt-5 text-white font-semibold hover:text-black rounded-sm border-2 border-white hover:border-green-400 hover:text-green-400 w-full max-w-[200px]">
+            Edit
+          </button>
+          <button className="btn mt-5 text-white font-semibold hover:text-black rounded-sm border-2 border-white hover:border-red-400 hover:text-red-400 w-full max-w-[200px]">
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );
